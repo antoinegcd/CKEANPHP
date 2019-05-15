a00ad12a4ce5c7f912b45d12fc66714e2ee58492
antoinegcd <antoine.gicquiaud@outlook.fr>
2019-05-15 23:32:17 +0200

    ajout affichage du code après le calcul. problème dans le calcul.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,9 @@ namespace App\Controller;
 
 use App\Formulaire\FormEanType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller
@@ -15,36 +17,29 @@ class HomeController extends Controller
      * @Route("/", name="home")
      */
     function index(Request $request){
-        $form = $this->createForm(FormEanType::class);
-        $form->handleRequest($request);
-        $ean = "";
-        if ($form->isSubmitted() && $form->isValid()){
-            $ean = $form->getExtraData();
+        return $this->render("base.html.twig", []);
+        }
 
-            $split = str_split((int)$ean, 1);
+    /**
+     * @Route("/response", name="response", methods={"GET"})
+     * @return int
+     */
+        function response(Request $request){
+            $ean = $request->get("ean");
             $somme = 0;
-            $keyResponse = 0;
-            foreach ($split as $key=>$value){
-                if (is_float(($key+1)/2)){
-                    $somme += $value;
-                }
-                else{
-                    $somme += $value*3;
+            for ($i = 0; $i < 11; $i++ ){
+                if (($i+1)%2 == 0){
+                    $somme += ((int)$ean[$i])*3;
+                }else{
+                    $somme += (int)$ean[$i];
                 }
             }
-            if ($somme%10 == 0)
+            $reste = $somme % 10;
+            if ($reste == 0){
                 $keyResponse = 0;
-            else
-                $keyResponse = 10-(somme%10);
-
-            return $this->render('base.html.twig', array(
-                "form" => $form->createView(),
-                "response" => $keyResponse,
-            ));
+            }else{
+                $keyResponse = 10-($reste);
+            }
+            return new JsonResponse(json_encode($keyResponse));
         }
-        return $this->render("base.html.twig",[
-            "form" => $form->createView(),
-            "response" => ""
-        ]);
-    }
 }
